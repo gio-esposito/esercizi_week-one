41f0d9e67fc7d1c898572a8336035e73aa59eb31
giovanni <giovanni@test.com>
2021-10-10 09:55:38 +0200

    Modificato commento a riga 7

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ print("Welcome to the Number Guess Game")
 print("I am thinking of a number between 1 and 100")
 
 secret_value = random.randint(1, 100)
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+difficulty = input("Choose a difficulty level. Type 'easy' or 'hard': ")
 guessed = False
 
 def compare():
