d279df57ac1b1fb572a0c6c6f606f303d8fc6ce0
giovanni <giovanni@test.com>
2021-10-10 09:06:25 +0200

    Primo commit di Guess Number

---
+++ test.py
@@ -0,0 +1,43 @@
+import random
+
+print("Welcome to the Number Guess Game")
+print("I am thinking of a number between 1 and 100")
+
+secret_value = random.randint(1, 100)
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+guessed = False
+
+def compare():
+    global secret_value
+    global guessed
+    guess_value = int(input("Make a guess: "))
+    if guess_value == secret_value:
+        print("You won")
+        guessed = True
+    elif guess_value < secret_value:
+        print("To low.")
+    elif guess_value > secret_value:
+        print("To high.")
+
+if difficulty == 'easy':
+    i = 0
+    contatore = 10
+    while i in range(10) and guessed == False:
+        compare()
+        contatore -= 1
+        i += 1
+        if contatore > 0 and guessed == False:
+            print(f"You have {contatore} tentatives left")
+        elif contatore == 0 and guessed == False:
+            print("You lose")
+elif difficulty == 'hard':
+    j = 0
+    contatore = 3
+    while j in range(3) and guessed == False:
+        compare()
+        contatore -= 1
+        j += 1
+        if contatore > 0 and guessed == False:
+            print(f"You have {contatore} tentatives left")
+        elif contatore == 0 and guessed == False:
+            print("You lose")
